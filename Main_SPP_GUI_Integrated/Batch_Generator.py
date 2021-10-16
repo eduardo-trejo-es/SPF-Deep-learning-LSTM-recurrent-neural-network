28f72b81971106741f8a414d264e53d0c5aaad0a
Alfredo Trejo <74158121+eduardo-trejo-es@users.noreply.github.com>
2021-10-15 20:42:49 -0500

    Test to see if changes in github get update in VS
    Fail, VS code just create a local copy of repository

--- Main_SPP_GUI_Integrated/Batch_Generator.py
+++ Main_SPP_GUI_Integrated/Batch_Generator.py
@@ -20,7 +20,7 @@ from PyQt5.QtCore import *
 
 
 #_--------------------------------------------------------
-class  WorkerThread_Batch_Gen(QThread):#Verifying if changes in github, appears in VS code
+class  WorkerThread_Batch_Gen(QThread):
     Update_Progress= pyqtSignal(int)
     Update_Progress_Description= pyqtSignal(str)
     LastBatchPathPredict=pyqtSignal(str)
